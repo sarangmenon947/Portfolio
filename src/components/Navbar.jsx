15bc5e5fd97735711cb06fa8d055cba2953bd3c8
sarangmenon947 <sarangmenon2011@gmail.com>
2025-12-17 06:35:44 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
                         ))}
                         <div className="flex space-x-4">
                             <a
-                                href="https://github.com/codingmastr"
+                                href="https://github.com/sarangmenon947"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="text-gray-300 hover:text-white"
